0be47d308c7302196d30e5ed713dbcbdd9e00350
Deepthi <deepsvit3450@gmail.com>
2023-08-02 20:44:03 -0700

    Functions

---
+++ Day2/Functions.py
@@ -0,0 +1,22 @@
+#Functions
+def add():
+     a=10
+     b=20
+     print(a+b)
+add()
+def sub():
+    a=50
+    b=30
+    print(a-b)
+sub()
+def multi():
+    x=10.5
+    y=8
+    print(x*y)
+multi()
+def dev():
+    x=89
+    y=35
+    print(x/y)
+dev()
+
