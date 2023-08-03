68ee583adce8052df39c0f4cf8e0e462a93f33d2
Deepthi <deepsvit3450@gmail.com>
2023-08-02 21:13:11 -0700

    Functionswithreturn

---
+++ Day2/Functions parameters.py
@@ -0,0 +1,14 @@
+def add(a,b):
+    print(a+b)
+def sub(a,b):
+    print(a-b)
+def multi(a,b):
+    print(a*b)
+def div(a,b):
+    print(a/b)
+add(30,20)
+multi(30,40)
+sub(20,10)
+
+
+
---
+++ Day2/FunctionwithReturn.py
@@ -0,0 +1,10 @@
+def add(a,b):
+    return(a+b)
+def sub(a,b):
+    return(a-b)
+def multi(a,b):
+    return(a*b)
+
+print(add(1,2))
+print(sub(10,50))
+print(multi(10,20))
